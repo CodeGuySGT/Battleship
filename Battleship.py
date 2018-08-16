60a52b3ad551403d7c9acc6d3c175da1457ed76a
Sherrick Glenn Thuesmunn <sherrick.thuesmunn@gmail.com>
2018-08-16 16:57:16 -0700

    Make a few cosmetic changes, add comments, add code to handle non-integer user input

--- Battleship.py
+++ Battleship.py
@@ -1,48 +1,70 @@
+"""
+8/16/2018
+Project from Codecademy. The project thus far was basically just following
+hand-holding instructions, but I don't like some of what they did and want
+to expand the program to include other cool features.
+"""
+
 from random import randint
+board = []                        #Will hold the board values
 
-board = []
-
+# Appends 5 lists of O's, so that when printed in a loop it forms a grid
 for x in range(5):
   board.append(["O"] * 5)
 
+# Loop that prints the board as a grid
 def print_board(board):
   for row in board:
-    print " ".join(row)
+    print(" ".join(row))
 
+# Functions that enerate a random position for the computer's battleship
 def random_row(board):
   return randint(0, len(board) - 1)
-
 def random_col(board):
   return randint(0, len(board[0]) - 1)
 
+# prints the intial message & board position with battleship hidden
+print("Welcome to Battleship, the game where you try to sink my Battleship.")
+print("")
 print_board(board)
 
+# Generating the actual super-secret battleship
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
 
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
+# debugging lul
+print(ship_row + 1)
+print(ship_col + 1)
 
+# Loop that handles the execution of the actual game
 for turn in range(4):
-  print("Turn %s" % (turn + 1))
-  guess_row = int(raw_input("Guess Row: "))
-  guess_col = int(raw_input("Guess Col: "))
+  # Getting the user's guess for this turn
+  print("Turn #%s" % (turn + 1))
+  guess_row = input("Guess Row: ")
+  guess_col = input("Guess Col: ")
 
-  if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sunk my battleship!"
+  # Makes sure user input is an int or float.
+  if guess_row.isdigit() == False or guess_col.isdigit() == False:
+    guess_row = 10                  # If not, changes value to 10
+    guess_col = 10                  #so it doesn't break code
+  else:                             # Changes number to string if it's a number
+    guess_row = int(guess_row) - 1
+    guess_col = int(guess_col) - 1
+
+  # Determining whether the user wins, loses, or tries again
+  if guess_row == ship_row and guess_col == ship_col:  # Wins
+    print("Congratulations! You sunk my battleship!")
     break
-  else:
+  else:                                                # Doesn't Win
     if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-      print "Oops, that's not even in the ocean."
-    elif(board[guess_row][guess_col] == "X"):
-      print "You guessed that one already."
+      print("Oops, that's not even in the ocean. Did you enter an integer \
+from 1 to 5?")
+    elif board[guess_row][guess_col] == "X":
+      print("You guessed that one already.")
     else:
-      print "You missed my battleship!"
+      print("You missed my battleship!")
       board[guess_row][guess_col] = "X"
-    if turn == 3:
+    if turn == 3:                                      # Loses
       print("Game Over")
-  # Print (turn + 1) here!
-  print(turn + 1)
+  print("")
   print_board(board)
